34c719d3aa108fef836d99007402d3531946c95f
Andrei <112513275+Andreirl032@users.noreply.github.com>
2023-02-26 01:28:04 -0300

    Criada interface para o programa
    Criada interface com o PySimpleGUI, e agora os vídeos e thumbnails são baixados na maior qualidade possível. Além disso, agora também é possível baixar áudios

--- pytubeApp.py
+++ pytubeApp.py
@@ -1,27 +1,53 @@
 import pytube
 import urllib.request
-import cv2
+import PySimpleGUI as sg
 
 def download_image(url, file_path, file_name):
     full_path = file_path + file_name + '.jpg'
     urllib.request.urlretrieve(url, full_path)
 
-option=int(input("Digite 1 para baixar vídeo e 2 para baixar thumbnail:\n"))
-link=str(input("Coloque uma URL de vídeo do Youtube:\n"))
-yt=pytube.YouTube(link)
-imglink=yt.thumbnail_url
-action=""
-if option==1:
-    yt.streams.filter(file_extension='mp4',only_video=True,res="720p",progressive=True).first().download()
-    action=" video from"
-elif option==2:
-    filename=str(input("Insira nome do arquivo:\n"))
-    filepath="C:/Users/andre/Downloads/Python YT Download/"
-    download_image(imglink,filepath,filename)
-    image = cv2.imread(filepath+filename+'.jpg')
-    crop_image = image[60:420, 0:640]
-    cv2.imwrite(filename+'.jpg',crop_image)
-    action="thumbnail from"
-    
+layout = [
+    [sg.Text("DOWNLOAD DE COMPONENTES DO YOUTUBE")], 
+    [sg.Combo(["Vídeo","Áudio","Thumbnail"],default_value="",key="-COMBO-",readonly=True)],
+    [sg.Text("Insira link para baixar vídeo do youtube")], 
+    [sg.In(size=(25, 1), enable_events=True, key="-INPUTLINK-")],
+    [sg.Text("Insira nome para baixar thumbnail do youtube (SE FOR BAIXAR THUMBNAIL)")], 
+    [sg.In(size=(25, 1), enable_events=True, key="-INPUTFILENAME-")],
+    [sg.Button("OK",key="-BUTTON-")],
+    [sg.Text("",key="-ERRORTEXT-")]
+    ]
 
-print(f'Downloaded{action}',link)
+# Create the window
+window = sg.Window(title="Python YouTube downloader - By Andrei", layout=layout,margins=(500,300))
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == sg.WIN_CLOSED:
+        break
+    if event=="-BUTTON-" and values["-COMBO-"]!="":
+        try:
+            link=values["-INPUTLINK-"]
+            yt=pytube.YouTube(link)
+            imglink=yt.thumbnail_url.replace("sddefault","maxresdefault")
+            action=""
+            if values["-COMBO-"]=="Vídeo":
+                yt.streams.get_highest_resolution().download()
+                action="vídeo MP4 de"
+            elif values["-COMBO-"]=="Áudio":
+                yt.streams.filter(only_audio=True).first().download()
+                action="áudio MP3 de"
+            elif values["-COMBO-"]=="Thumbnail":
+                filename=values["-INPUTFILENAME-"]
+                filepath="C:/Users/andre/Downloads/Python YT Download/"
+                download_image(imglink,filepath,filename)
+                action="thumbnail JPG de"
+            print(f'Você baixou {action}',link)
+        except:
+            window["-ERRORTEXT-"].update("ERRO. INSIRA UM LINK VÁLIDO")
+        else:
+            window["-ERRORTEXT-"].update("")
+
+window.close()
